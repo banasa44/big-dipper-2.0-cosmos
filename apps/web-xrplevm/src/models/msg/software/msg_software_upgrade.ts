f088901e65cbea6d198a6e175b525773de6d9981
Longbo <longboqiu@hotmail.com>
2024-12-20 10:31:04 +0100

    [web-xrplevm] fix: category

--- apps/web-xrplevm/src/models/msg/software/msg_software_upgrade.ts
+++ apps/web-xrplevm/src/models/msg/software/msg_software_upgrade.ts
@@ -1,15 +1,3 @@
-// {
-// 	"plan": {
-// 		"info": "{\"binaries\":{\"linux/amd64\":\"https://github.com/Peersyst/exrp/releases/download/v5.0.0/node_5.0.0_Linux_amd64.tar.gz\",\"linux/arm64\":\"https://github.com/Peersyst/exrp/releases/download/v5.0.0/node_5.0.0_Linux_arm64.tar.gz\",\"darwin/amd64\":\"https://github.com/Peersyst/exrp/releases/download/v5.0.0/node_5.0.0_Darwin_amd64.tar.gz\",\"darwin/arm64\":\"https://github.com/Peersyst/exrp/releases/download/v5.0.0/node_5.0.0_Darwin_arm64.tar.gz\"}}",
-// 		"name": "v5.0.0",
-// 		"time": "0001-01-01T00:00:00Z",
-// 		"height": "13242557",
-// 		"upgraded_client_state": null
-// 	},
-// 	"@type": "/cosmos.upgrade.v1beta1.MsgSoftwareUpgrade",
-// 	"authority": "ethm10d07y265gmmuvt4z0w9aw880jnsr700jpva843"
-// }
-
 import { Categories } from '@/models/types';
 
 /* eslint-disable */
@@ -46,7 +34,6 @@ class MsgSoftwareUpgrade {
     authority: string,
     json: any
   ) {
-    console.log('MsgSoftwareUpgrade - constructor', plan);
     this.type = type;
     this.category = category;
     this.plan = plan;
@@ -57,7 +44,7 @@ class MsgSoftwareUpgrade {
   static fromJson(object: any): MsgSoftwareUpgrade {
     const message = {} as MsgSoftwareUpgrade;
 
-    message.category = 'xrpl-evm';
+    message.category = 'software';
     message.authority = object.authority;
     message.type = object['@type'];
     message.json = object;
